9af5152c5b67fe36655721694776f455e9399b31
sunny <sunnysingh21978@gmail.com>
2023-10-02 15:56:19 +0530

    ..

--- src/components/intro/Intro.jsx
+++ src/components/intro/Intro.jsx
@@ -18,7 +18,7 @@ export default function Intro() {
     <div className="intro" id="intro">
       <div className="left">
         <div className="imgcontainer">
-          <img src="assets/sunny-photo.jpeg" alt="" />
+          <img src="assets/sunny-photo1.jpeg" alt="" />
         </div>
       </div>
       <div className="right">
